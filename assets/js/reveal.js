415e1638b0a95e943c69b4803f8c2d0cdab00a46
ArthurSantos <98977659+ArthurEzequiel@users.noreply.github.com>
2024-03-12 11:24:39 -0300

    update

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -6,3 +6,66 @@ ScrollReveal().reveal('.bottom-reveal-2000', {
     distance: '0%',
     origin: 'bottom',
 });
+
+ScrollReveal().reveal('.bottom-reveal-1500', {
+    delay: 100,
+    duration: 2500,
+    reset: false,
+    distance: '5%',
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.bottom-reveal-1500-400', {
+    delay: 500,
+    duration: 2500,
+    reset: false,
+    distance: '5%',
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.bottom-reveal-1500-600', {
+    delay: 800,
+    duration: 2500,
+    reset: false,
+    distance: '5%',
+    origin: 'bottom',
+});
+
+
+ScrollReveal().reveal('.delay100', {
+    delay: 100,
+});
+
+ScrollReveal().reveal('.delay200', {
+    delay: 200,
+});
+
+ScrollReveal().reveal('.delay300', {
+    delay: 300,
+});
+
+ScrollReveal().reveal('.delay400', {
+    delay: 400,
+});
+
+ScrollReveal().reveal('.delay500', {
+    delay: 500,
+});
+
+ScrollReveal().reveal('.delay600', {
+    delay: 600,
+});
+
+ScrollReveal().reveal('.delay700', {
+    delay: 700,
+});
+ScrollReveal().reveal('.delay800', {
+    delay: 800,
+});
+
+ScrollReveal().reveal('.delay900', {
+    delay: 900,
+});
+ScrollReveal().reveal('.delay1000', {
+    delay: 1000,
+});
